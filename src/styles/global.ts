dcfa3303c9a913466dd0b0545e26d7d4a8af2531
Eduardo <edu-henrique2014@hotmail.com>
2021-07-01 18:38:42 -0300

    Initial commit 🚀

---
+++ src/styles/global.ts
@@ -0,0 +1,50 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  :root {
+    --gray-900: #292929;
+    --gray-800: #414141;
+    --gray-700: #4F4F4F;
+    --gray-500: #8F8F8F;
+    --white: #FFFFFF;
+
+    --green-500: #28DF99;
+  }
+  
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+  @media (max-width:1080px) {
+    html {
+      font-size: 93.75%;
+    }
+  }
+
+  @media (max-width:720px) {
+    html {
+      font-size: 87.5%;
+    }
+  }
+
+  body {
+    background: var(--gray-900);
+    color: var(--white);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, select, button {
+    font: 400 1rem "Roboto", sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
