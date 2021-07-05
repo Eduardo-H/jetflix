ad4ad1d07175ba987a42ab9068559e2624cb24d6
Eduardo <edu-henrique2014@hotmail.com>
2021-07-05 18:36:27 -0300

    Custom fonts modified

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;600;700;900&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet" />
 
           <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         </Head>
